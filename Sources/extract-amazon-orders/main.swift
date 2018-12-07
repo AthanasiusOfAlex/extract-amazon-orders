137e6db6c1f7ec459dd0bcaf31ef10ed5cdf0bd2
AthanasiusOfAlex <lmelahn@legionaries.org>
2018-12-07 21:31:28 +0100

    ready to go

--- Sources/extract-amazon-orders/main.swift
+++ Sources/extract-amazon-orders/main.swift
@@ -1,1 +1,32 @@
-print("Hello, world!")
+import Foundation
+import AmazonOrderExtractor
+
+let outputFile: URL
+
+// MARK - deal with command line
+do {
+    if CommandLine.arguments.count <= 1 {
+        outputFile = URL(fileURLWithPath: "/dev/stdout")
+    } else {
+        outputFile = URL(fileURLWithPath: CommandLine.arguments[1])
+    }
+}
+
+//// MARK - Check to see if the file exists.
+//do {
+//    func fileExists() -> Bool {
+//        let url = outputFile.absoluteURL.deletingLastPathComponent()
+//        let fileManager = FileManager.default
+//        
+//        print(url.absoluteString)
+//        print(fileManager.fileExists(atPath: url.absoluteString))
+//        return fileManager.fileExists(atPath: url.absoluteString)
+//    }
+//    
+//    guard fileExists() else { exit(1) }
+//}
+
+// Print out the links
+do {
+    printListOfLinks(outFileUrl: outputFile)
+}
